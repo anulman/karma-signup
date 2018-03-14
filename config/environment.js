a327a760c86a96c9503c8c76c8838d14f88cee64
@anulman <anulman@gmail.com>
2018-03-14 16:12:56 -0400

    refactor(pods): use pods structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'karma-signup',
+    podModulePrefix: 'karma-signup/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
