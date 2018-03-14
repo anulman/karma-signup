jacobq/cook-for-mom

chore(prember): add sitemap util

ember-cli-build.js
@@ -11,6 +11,7 @@ module.exports = function(defaults) {
       'insertEmberWormholeElementToDom': false
     },
     prember: {
+      baseRoot: 'https://cookformom.com',
       urls: [
         '/'
       ]

anulman/karma-signup

ember-cli-build.js
@@ -11,6 +11,7 @@ module.exports = function(defaults) {
       'insertEmberWormholeElementToDom': false
     },
     prember: {
+      baseRoot: 'https://karmacoop.org',
       urls: [
         '/'
       ]