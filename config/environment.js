35c35b32922d7bb2c37018e235ed1cc734edcfe5
@anulman <anulman@gmail.com>
2018-03-14 15:36:47 -0400

    chore(deps): add concurrency, fingerprint tools, firebase, and md5

--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,43 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    metricsAdapters: [{
+      name: 'GoogleAnalytics',
+      environments: ['development', 'production'],
+      config: {
+        id: 'UA-57942215-1',
+        debug: environment === 'development',
+        trace: environment === 'development',
+        sendHitTask: environment !== 'development'
+      },
+    }, {
+      name: 'Segment',
+      environments: ['production'],
+      config: {
+        key: 'BUfXIjvNXZbqj9PFnofOL1t8ZQaHlKh4'
+      }
+      // todo do we have facebook pixel?
+    // }, {
+      // name: 'FacebookPixel',
+      // environments: ['production'],
+      // config: {
+        // id: ''
+      // }
+    }],
+
+    sentry: {
+      dsn: 'https://d2c7984596214f0694da81ca20dc8e90@sentry.io/303935'
+    },
+
+    firebase: {
+      apiKey: "AIzaSyBkk_vXl136w9P-7CpEOksiTo8hk81ceOU",
+      authDomain: "karma-signups.firebaseapp.com",
+      databaseURL: "https://karma-signups.firebaseio.com",
+      projectId: "karma-signups",
+      storageBucket: "karma-signups.appspot.com",
+      messagingSenderId: "576293640418"
     }
   };
 
@@ -45,7 +82,12 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV['ember-full-story'] = {
+      enabledEnvironments: ['production'],
+      host: 'www.fullstory.com',
+      namespace: 'FS',
+      org: 'B22AD'
+    }
   }
 
   return ENV;
