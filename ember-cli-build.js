3951b396cfdd8d86346b8c50ccbd8200a2202080
@anulman <anulman@gmail.com>
2018-07-20 13:02:01 -0400

    chore(ember-bootstrap): ensure wormhole is inserted to dom

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,7 +8,7 @@ module.exports = function(defaults) {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
       'importBootstrapCSS': false,
-      'insertEmberWormholeElementToDom': false
+      'insertEmberWormholeElementToDom': true
     },
     prember: {
       baseRoot: 'https://karmacoop.org',
